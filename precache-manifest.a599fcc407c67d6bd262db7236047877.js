c7b8ef8462e0af36b4724ee8a170aff44514b7f1
Ari Bahtiar <aribahtiar@unsil.ac.id>
2020-06-07 17:30:08 +0700

    start

---
+++ precache-manifest.a599fcc407c67d6bd262db7236047877.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbb5170a4ccbfc96a32",
+    "url": "css/app.36c93714.css"
+  },
+  {
+    "revision": "17588f3d4546eda84d87",
+    "url": "css/c-alert.42e7a06f.css"
+  },
+  {
+    "revision": "cf2c891741b407267acf5e70c3646d73",
+    "url": "img/logo-bootwind-template-sm.cf2c8917.png"
+  },
+  {
+    "revision": "73dd23cc695a14e501b2ef3d9da2ba05",
+    "url": "img/logo-bootwind-template.73dd23cc.png"
+  },
+  {
+    "revision": "79df8aaf8b3e7055b05c6197f5d4bbff",
+    "url": "index.html"
+  },
+  {
+    "revision": "72b97a76a1e3bee1e3a5",
+    "url": "js/about-legacy.98099b61.js"
+  },
+  {
+    "revision": "efbb5170a4ccbfc96a32",
+    "url": "js/app-legacy.2f5864b4.js"
+  },
+  {
+    "revision": "17588f3d4546eda84d87",
+    "url": "js/c-alert-legacy.b2be67b9.js"
+  },
+  {
+    "revision": "e5183da4c13e48a5fb12",
+    "url": "js/chunk-vendors-legacy.b49c8735.js"
+  },
+  {
+    "revision": "5d100c8a16095291ed0f50a399f09984",
+    "url": "manifest.json"
+  }
+]);
