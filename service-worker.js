75762acfd85b36c5f6ac3e908945a0fb931286a8
unknown <aribahtiar@unsil.ac.id>
2020-06-11 01:21:57 +0700

    update doc

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.02ecf8837e4e8a46fba5b47e70523384.js"
+  "precache-manifest.e95ec9fe52291d0f4fd5383815d993fc.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-bootwind-template"});
